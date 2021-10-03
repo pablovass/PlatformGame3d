6b2150c77f0f9b338b35d1fe89091fa6d7401741
Pablo Vallejos <faktory.fm@live.com>
2021-10-03 15:21:57 -0300

    end prototipe

---
+++ Assets/_Scripts/Element/Spin.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float translateSpeed = 1;
+    public float rotateSpeed = 60;
+    // Update is called once per frame
+    void Update()
+    {   
+       // transform.Translate(Vector3.right*translateSpeed*Time.deltaTime);
+       transform.localPosition+=Vector3.left*translateSpeed*Time.deltaTime; 
+       transform.Rotate(Vector3.up*rotateSpeed*Time.deltaTime);
+    }
+}
--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -2,14 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Random = System.Random;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 
 {
+    private const string SPEED_F = "Speed_f";
     [SerializeField]private float jumpForce = 5;
-    [SerializeField] private float gravityMultiplier;
+    [SerializeField] private float gravityMultiplier=1;
     private bool isOnGround = true; 
     //public bool gameOver = false; always is falses
     private bool _gameOver;
@@ -18,28 +20,39 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
 
     private Animator _animator;
-    
-
+    private float SpeedMultiplier;
     private bool IsMovingJump = true;
+    public ParticleSystem explosion;
+    public ParticleSystem dirt;
+    public AudioClip jumpSound, crashSound;
+
+    private AudioSource _audioSource;
+   
     // Start is called before the first frame update
+    
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        Physics.gravity *= gravityMultiplier;
-        
+        Physics.gravity = gravityMultiplier * new Vector3(0, -9.81f, 0);
         _animator = GetComponent<Animator>();
-       
+        _animator.SetFloat(SPEED_F,1);
+        _audioSource = GetComponent<AudioSource>();
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        SpeedMultiplier += Time.deltaTime/10;
+        
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !GameOver)
         {
+            
             playerRb.AddForce(Vector3.up*jumpForce,ForceMode.Impulse); //F=m*a
             IsMovingJump = !IsMovingJump;
             _animator.SetTrigger("Jump_trig");
+            _audioSource.PlayOneShot(jumpSound,1);
             isOnGround = false;
         } 
         
@@ -49,16 +62,31 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Ground")) //SOLO SALTA SI esta en el suelo
         {
-            isOnGround = true;    
-        }
-
-        if (other.gameObject.CompareTag("Obstacle"))
+            if (!GameOver)
+            {
+                isOnGround = true;
+                dirt.Play();
+            }
+                
+        } else if (other.gameObject.CompareTag("Obstacle"))
         {
             _gameOver = true;
             Debug.Log("Game Over");
+            explosion.Play();
             _animator.SetBool("Death_b",true);
             _animator.SetInteger("DeathType_int",1);
+            dirt.Stop();
+            _audioSource.PlayOneShot(crashSound,Random.Range(1,3));
+            Invoke("RestartGame",1.0f);
+            
         }
         
     }
-}
+
+    void RestartGame()
+    {
+        SpeedMultiplier = 1;
+       SceneManager.LoadSceneAsync("Prototype 3",LoadSceneMode.Single);
+        
+    }
+} 
